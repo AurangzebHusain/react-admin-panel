4620394e69b77cc864c01b59c7afce934130c81e
AurangzebHusain <aurangzeb.husain@adroit-vantage.com>
2022-08-07 23:48:40 +0530

    Added Line chart

--- src/components/charts/LineChart.tsx
+++ src/components/charts/LineChart.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Legend,
+  LineSeries,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+import {
+  lineCustomSeries,
+  LinePrimaryXAxis,
+  LinePrimaryYAxis,
+} from "../../data/dummy";
+import { StateContext } from "../../contexts/contextProvider";
 
 const LineChart = () => {
-  return <div>LineChart</div>;
+  const { currentMode } = useContext(StateContext);
+  return (
+    <ChartComponent
+      id="line-chart"
+      height="420px "
+      // primaryXAxis={LinePrimaryXAxis}
+      // primaryYAxis={LinePrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === "Dark" ? "#33373E" : "#fff"}
+    >
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item}></SeriesDirective>
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
 };
 
 export { LineChart };
--- src/pages/charts/Line.tsx
+++ src/pages/charts/Line.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Header, LineChart } from "../../components";
 
 const Line = () => {
-  return <div>Line</div>;
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg roundel-3xl">
+      <Header title="Inflation Rate " category={"Chart"} />
+      <LineChart />
+    </div>
+  );
 };
 
 export { Line };
